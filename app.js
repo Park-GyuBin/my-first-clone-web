c2fdc2adf367399a051064a239da9c7d32ebe71f
Park-GyuBin <101565860+Park-GyuBin@users.noreply.github.com>
2022-08-17 15:08:53 +0900

    start-practice

--- app.js
+++ app.js
@@ -1,2 +1,9 @@
-const player={name:"gyub", look:"handsome"}
-console.log(player.look);
+const loginForm = document.querySelector(".login-form");
+const loginText = loginForm.querySelector("input");
+const loginBtn = loginForm.querySelector("button");
+
+function clickBtn() {
+    console.log(loginText.value);
+}
+
+loginBtn.addEventListener("click",clickBtn)
